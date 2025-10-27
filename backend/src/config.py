97ed564c169f8c7421bfb353adab892f82c1e093
Akshay Nagamalla <akshaynagamalla23@gmail.com>
2025-10-27 23:31:36 +0530

    feat: add S3 credentials config

--- backend/src/config.py
+++ backend/src/config.py
@@ -9,5 +9,15 @@ class Settings:
     DB_HOST: str = os.getenv("DB_HOST")
     DB_PORT: int = os.getenv("DB_PORT")
     DB_NAME: str = os.getenv("DB_NAME") 
+    SUPABASE_URL: str = os.getenv("SUPABASE_URL") 
+    SUPABASE_KEY: str = os.getenv("SUPABASE_KEY") 
+
+    BUCKET_NAME: str = os.getenv("BUCKET_NAME")
+    S3_ENDPOINT: str = os.getenv("S3_ENDPOINT")
+    S3_REGION: str = os.getenv("S3_REGION")
+    S3_ACCESS_KEY_ID: str = os.getenv("S3_ACCESS_KEY_ID")
+    S3_SECRET_ACCESS_KEY: str = os.getenv("S3_SECRET_ACCESS_KEY")
+    
+
 
 settings = Settings()
